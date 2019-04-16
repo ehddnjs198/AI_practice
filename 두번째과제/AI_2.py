d7565431f390c12cb7d5e5e045fcdc45f0a31e9a
ehddnjs198 <35540229+ehddnjs198@users.noreply.github.com>
2019-04-16 21:47:54 +0900

    Update AI_2.py

--- 두번째과제/AI_2.py
+++ 두번째과제/AI_2.py
@@ -1,7 +1,4 @@
 import os
-
-TITANIC_PATH = os.path.join("datasets", "titanic")
-
 import pandas as pd
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.pipeline import Pipeline
@@ -12,7 +9,12 @@ from sklearn.svm import SVC
 from sklearn.model_selection import cross_val_score
 from sklearn.ensemble import RandomForestClassifier
 from matplotlib import pyplot as plt
-from sklearn.metrics import precision_score, recall_score
+from sklearn.metrics import precision_score, recall_score ,accuracy_score
+import numpy as np
+from sklearn.model_selection import GridSearchCV
+from sklearn.ensemble import RandomForestRegressor
+
+TITANIC_PATH = os.path.join("datasets", "titanic")
 
 def load_titanic_data(filename, titanic_path=TITANIC_PATH):
     csv_path = os.path.join(titanic_path, filename)
@@ -34,63 +36,111 @@ class MostFrequentImputer(BaseEstimator, TransformerMixin):
     def transform(self, X, y=None):
         return X.fillna(self.most_frequent_)
 
-def num_pipeline(data):
+def CreateTrainFeature(train_data):
+    train_data["FamilySize"] = train_data["SibSp"] + train_data["Parch"] + 1 # 자신을 포함해야하니 1을 더합니다
+    train_data["Fare"] = train_data["Fare"].map(lambda i: np.log(i) if i > 0 else 0)
+    train_data["Embarked"].fillna('S', inplace=True)
+
+    train_data['Age_cat'] = 0
+    train_data.loc[train_data['Age'] < 10, 'Age_cat'] = 0
+    train_data.loc[(10 <= train_data['Age']) & (train_data['Age'] < 20), 'Age_cat'] = 1
+    train_data.loc[(20 <= train_data['Age']) & (train_data['Age'] < 30), 'Age_cat'] = 2
+    train_data.loc[(30 <= train_data['Age']) & (train_data['Age'] < 40), 'Age_cat'] = 3
+    train_data.loc[(40 <= train_data['Age']) & (train_data['Age'] < 50), 'Age_cat'] = 4
+    train_data.loc[(50 <= train_data['Age']) & (train_data['Age'] < 60), 'Age_cat'] = 5
+    train_data.loc[(60 <= train_data['Age']) & (train_data['Age'] < 70), 'Age_cat'] = 6
+    train_data.loc[70 <= train_data['Age'], 'Age_cat'] = 7
+    return train_data
+
+def CreateTestFeature(test_data):
+   
+    
+    test_data["FamilySize"] = test_data['SibSp'] + test_data["Parch"] + 1 # 자신을 포함해야하니 1을 더합니다
+    
+    test_data["Fare"] = test_data["Fare"].map(lambda i: np.log(i) if i > 0 else 0)
+    
+    test_data['Age_cat'] = 0
+    test_data.loc[test_data['Age'] < 10, 'Age_cat'] = 0
+    test_data.loc[(10 <= test_data['Age']) & (test_data['Age'] < 20), 'Age_cat'] = 1
+    test_data.loc[(20 <= test_data['Age']) & (test_data['Age'] < 30), 'Age_cat'] = 2
+    test_data.loc[(30 <= test_data['Age']) & (test_data['Age'] < 40), 'Age_cat'] = 3
+    test_data.loc[(40 <= test_data['Age']) & (test_data['Age'] < 50), 'Age_cat'] = 4
+    test_data.loc[(50 <= test_data['Age']) & (test_data['Age'] < 60), 'Age_cat'] = 5
+    test_data.loc[(60 <= test_data['Age']) & (test_data['Age'] < 70), 'Age_cat'] = 6
+    test_data.loc[70 <= test_data['Age'], 'Age_cat'] = 7
+
+    return test_data
+
+def Explanation():
+    print("자질 설명")
+    print("FamilySize = Parch + SibSp 로 하나 만들어주었고")
+    print("Age_cat라는걸 나이는 10살 단위로 끊어서 하나의 자질을 만들어줬습니다.")
+    print("train_data의 Embarked의 빈공간을 다 S로 넣어주었습니다.")
+
+def PrintSVM(X_train, y_train,X_test,y_test):
+    print("SVM")
+    svm_clf = SVC(gamma="auto")
+    svm_clf.fit(X_train, y_train)
+    
+    y_pred = svm_clf.predict(X_test)
+    print("accuracy  = ",accuracy_score(y_test, y_pred))
+    print("recall    = ",recall_score(y_test,y_pred))
+    print("precision = ",precision_score(y_test,y_pred))
+
+def PrintRandomForest(X_train, y_train,X_test,y_test):
+    forest_clf = RandomForestClassifier(n_estimators=100, random_state=42)
+    forest_clf.fit(X_train, y_train)
+
+    y_pred = forest_clf.predict(X_test)
+
+    print("RandomForestClassifier")
+    print("accuracy  = ",accuracy_score(y_test, y_pred))
+    print("recall    = ",recall_score(y_test,y_pred))
+    print("precision = ",precision_score(y_test,y_pred))
+    
+if __name__ == "__main__":
+    
+    train_data = load_titanic_data("train.csv")
+    test_data = load_titanic_data("test.csv")
+    y_data = load_titanic_data("gender_submission.csv")
+
+    train_data = CreateTrainFeature(train_data)#train_data 자질 셋팅
+    test_data = CreateTestFeature(test_data)#test_data 자질 셋팅
+        
     num_pipeline = Pipeline([
-        ("select_numeric", DataFrameSelector(["Age", "SibSp", "Parch", "Fare"])),
+        ("select_numeric", DataFrameSelector(["Age_cat", "FamilySize", "Fare"])),
         ("imputer", SimpleImputer(strategy="median")),
     ])
-    num_pipeline.fit_transform(data)
-    return num_pipeline
 
-def cat_pipeline(data):
     cat_pipeline = Pipeline([
         ("select_cat", DataFrameSelector(["Pclass", "Sex", "Embarked"])),
         ("imputer", MostFrequentImputer()),
         ("cat_encoder", OneHotEncoder(sparse=False)),
     ])
-    cat_pipeline.fit_transform(data)
-    return cat_pipeline
-
-if __name__ == "__main__":
-    
-    train_data = load_titanic_data("train.csv")
-    test_data = load_titanic_data("test.csv")
 
     preprocess_pipeline = FeatureUnion(transformer_list=[
-        ("num_pipeline", num_pipeline(train_data)),
-        ("cat_pipeline", cat_pipeline(train_data)),
+        ("num_pipeline", num_pipeline),
+        ("cat_pipeline", cat_pipeline),
     ])
-    
+
     X_train = preprocess_pipeline.fit_transform(train_data)
     y_train = train_data["Survived"]
 
+    X_test = preprocess_pipeline.fit_transform(test_data)
+    y_test = y_data["Survived"]
+
+
+    Explanation()
+
+    print("===========================================")
     
+    PrintSVM(X_train, y_train,X_test,y_test)
     
-    svm_clf = SVC(gamma="auto")
-    svm_clf.fit(X_train, y_train)
-
-      
-    X_test = preprocess_pipeline.transform(test_data)
-    y_pred = svm_clf.predict(X_test)
-
+    print("===========================================")
     
-      
-    svm_scores = cross_val_score(svm_clf, X_test, y_pred, cv=10)
-    
-    print(y_pred)
-    
-##
-##    forest_clf = RandomForestClassifier(n_estimators=100, random_state=42)
-##    forest_scores = cross_val_score(forest_clf, X_train, y_train, cv=10)
-##    print(forest_scores.mean())
+    PrintRandomForest(X_train, y_train,X_test,y_test) 
 
-##    print("Precision: {:.2f}%".format(100 * precision_score(forest_scores, y_pred)))
-##    print("Recall: {:.2f}%".format(100 * recall_score(forest_scores, y_pred)))  
+   
+   
 
-
-
-
-
-
-
-
+        
